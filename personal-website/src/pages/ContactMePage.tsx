5bc00053bbcfc558051e9929a1a25b147b56fb8a
Linh <57379191+linh-pham19@users.noreply.github.com>
2025-04-13 14:30:26 -0400

    Add carousel (#5)
    * added banner. Can submit two messages in a row with no issues
    
    * added field validation as user types
    
    * carousel works

--- personal-website/src/pages/ContactMePage.tsx
+++ personal-website/src/pages/ContactMePage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import '../App.css'
 import emailjs from 'emailjs-com';
+import Banner from '../components/Banner';
 
 function ContactMePage() {
   // Define the type of error message
@@ -32,14 +33,75 @@ function ContactMePage() {
     phoneNumber: errorMessages.phoneNumber,
     message: errorMessages.message,
   } as ErrorMessages);
+  const [banner, setBanner] = useState(false)
+  const [isBlurred, setIsBlurred] = useState(false);
 
 
   const SERVICE_ID = import.meta.env.VITE_REACT_APP_EMAILJS_SERVICE_ID;
   const TEMPLATE_ID = import.meta.env.VITE_REACT_APP_EMAILJS_TEMPLATE_ID;
   const PUBLIC_KEY = import.meta.env.VITE_REACT_APP_EMAILJS_PUBLIC_KEY;
 
-  // Validate form fields and print out errors if any
-  const validateForm = () => {
+  // Validate form fields as user fills out fields
+  const validateField = (field: string, value: string) => {
+    // Create a copy of the errors object to avoid mutating the state directly
+    // and to ensure that we can reset the errors after submission
+    const newErrors = { ...errors };
+
+    switch (field) {
+      case 'name':
+        if (!value.trim()) {
+          newErrors.name = errorMessages.name;
+        } else {
+          delete newErrors.name;
+        }
+        // so we don't check for every single field onChange
+        break;
+
+      case 'email':
+        if (!value.trim()) {
+          newErrors.email = errorMessages.email;
+        } else if (!/\S+@\S+\.\S+/.test(email)) 
+          {
+          newErrors.email = 'Email is invalid.';
+        } else {
+          delete newErrors.email;
+        }
+        break;
+
+      case 'phoneNumber':
+        if (!value.trim()) {
+          newErrors.phoneNumber = errorMessages.phoneNumber;
+        } else if (phoneNumber && !/^\d{10}$/.test(phoneNumber)) {
+          newErrors.phoneNumber = 'Phone number must be 10 digits.';
+        } else {
+          delete newErrors.phoneNumber;
+        }
+        break;
+
+      case 'message':
+        if (!value.trim()) {
+          newErrors.message = errorMessages.message;
+        } else {
+          delete newErrors.message;
+        }
+        break;
+
+      case 'contactPreference':
+        if (!value) {
+          newErrors.contactPreference = 'Please select a contact preference';
+        } else {
+          delete newErrors.contactPreference;
+        }
+        break;
+
+      // if the field can't be recognized, we will ignore it
+      default:
+        break;
+    }
+    setErrors(newErrors);
+  }
+
+  const isFormValid = () => {
     const newErrors: { name?: string, email?: string, phoneNumber?: string, message?: string, contactPreference?: string } = {};
     if (!name.trim()) {
       newErrors.name = errorMessages.name;
@@ -66,6 +128,7 @@ function ContactMePage() {
     return Object.keys(newErrors).length === 0; // Return true if no errors
   }
 
+
   const handleContactPreferenceChange = (value: string) => {
     if (value === "No I don't want to be contacted") {
       window.confirm(
@@ -77,121 +140,151 @@ function ContactMePage() {
     setContactPreference(value); // Update the state if confirmed or for other options
   };
 
+
+
   // Handle form submission
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (validateForm()) {
-      alert('Thank you for sending me a message!');
-      // Clear form fields after successful submission
-      setName('');
-      setEmail('');
-      setMessage('');
-      setPhoneNumber('');
-      setContactPreference('');
-      setErrors({});
+    if (!isFormValid()) {
+      return;
     }
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, event.target as HTMLFormElement, PUBLIC_KEY)
-    .then((result) => {
-      console.log(result.text);
-      alert('Message Sent Successfully')
-    }, (error) => {
-      console.log(error.text);
-      alert('Something went wrong!')
-    });
-  (event.target as HTMLFormElement).reset()
+      .then(() => {
+        alert('Message Sent Successfully')
+      }, (error) => {
+        console.log(error.text);
+        alert('Something went wrong!')
+      });
+
+    // Reset all form fields
+    setName('');
+    setEmail('');
+    setPhoneNumber('');
+    setMessage('');
+    setContactPreference('');
+    // Clear all error messages
+    setErrors(errorMessages);
+
+
+    (event.target as HTMLFormElement).reset()
+    setBanner(true)
+    setIsBlurred(true);
   };
 
+  const handleBannerClose = () => {
+    setBanner(false);
+    setIsBlurred(false);
+  }
   return (
-    <div className="contact-page">
-      <h1 className="contact-title">Please leave me a message</h1>
-      <form onSubmit={handleSubmit} className="contact-form">
-        <label className="form-label">
-          Name:
-          <input
-            type="text"
-            name="name"
-            className="form-input"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          {errors.name && <p className="form-error" style={{ color: 'red' }}>{errors.name}</p>}
-        </label>
-        <br />
-        <label className="form-label">
-          Email:
-          <input
-            type="email"
-            name="email"
-            className="form-input"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {errors.email && <p className="form-error" style={{ color: 'red' }}>{errors.email}</p>}
-        </label>
-        <br />
-        <label className="form-label">
-          Phone number:
-          <input
-            type="phoneNumber"
-            name="phoneNumber"
-            className="form-input"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-          {errors.email && <p className="form-error" style={{ color: 'red' }}>{errors.phoneNumber}</p>}
-        </label>
-        <label className="form-label">
-          Message:
-          <textarea
-            name="message"
-            className="form-textarea"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          {errors.message && <p className="form-error" style={{ color: 'red' }}>{errors.message}</p>}
-        </label>
-        <br />
-        <label className="form-label">
-          Would you like to be contacted via phone or email?
-        </label>
-        {errors.contactPreference && <p className="form-error" style={{ color: 'red' }}>{errors.contactPreference}</p>}
-        <div className="label-wrapper">
+    <>
+      {banner && <Banner onClose={handleBannerClose} />}
+      <div className="contact-page" style={{ filter: isBlurred ? 'blur(5px)' : 'none' }}>
+        <h1 className="contact-title">Please leave me a message</h1>
+        <form onSubmit={handleSubmit} className="contact-form">
+          {!validateField && <p style={{ color: 'red' }}>Please see error(s) below</p>}
           <label className="form-label">
+            Name:
             <input
-              type="radio"
-              name="contactPreference"
-              value="Phone"
-              checked={contactPreference === 'Phone'}
-              onChange={(e) => handleContactPreferenceChange(e.target.value)}
+              type="text"
+              name="name"
+              className="form-input"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+                validateField('name', e.target.value)
+              }}
             />
-            Phone
+            {errors.name && <p className="form-error" style={{ color: 'red' }}>{errors.name}</p>}
           </label>
-          <label className="form-label" style={{ marginLeft: '1rem' }}>
+          <br />
+          <label className="form-label">
+            Email:
             <input
-              type="radio"
-              name="contactPreference"
-              value="Email"
-              checked={contactPreference === 'Email'}
-              onChange={(e) => handleContactPreferenceChange(e.target.value)}
+              type="email"
+              name="email"
+              className="form-input"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+                validateField('email', e.target.value)
+              }}
             />
-            Email
+            {errors.email && <p className="form-error" style={{ color: 'red' }}>{errors.email}</p>}
           </label>
-          <label className="form-label" style={{ marginLeft: '1rem' }}>
+          <br />
+          <label className="form-label">
+            Phone number:
             <input
-              type="radio"
-              name="contactPreference"
-              value="No I don't want to be contacted"
-              checked={contactPreference === 'No I don\'t want to be contacted'}
-              onChange={(e) => handleContactPreferenceChange(e.target.value)}
+              type="text"
+              name="phoneNumber"
+              className="form-input"
+              value={phoneNumber}
+              onChange={(e) => {
+                setPhoneNumber(e.target.value)
+                validateField('phoneNumber', e.target.value)
+              }}
             />
-            I don't want to be contacted
+            {errors.phoneNumber && <p className="form-error" style={{ color: 'red' }}>{errors.phoneNumber}</p>}
           </label>
-        </div>
-        <br />
-        <button type="submit" className="form-button">Submit</button>
-      </form>
-    </div>
+          <label className="form-label">
+            Message:
+            <textarea
+              name="message"
+              className="form-textarea"
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value)
+                validateField('message', e.target.value)
+              }}
+            />
+            {errors.message && <p className="form-error" style={{ color: 'red' }}>{errors.message}</p>}
+          </label>
+          <br />
+          <label className="form-label">
+            Would you like to be contacted via phone or email?
+          </label>
+          {errors.contactPreference && <p className="form-error" style={{ color: 'red' }}>{errors.contactPreference}</p>}
+          <div className="label-wrapper">
+            <label className="form-label">
+              <input
+                type="radio"
+                name="contactPreference"
+                value="Phone"
+                checked={contactPreference === 'Phone'}
+                onChange={(e) => {
+                  handleContactPreferenceChange(e.target.value)
+                  validateField('contactPreference', e.target.value)
+                }}
+              />
+              Phone
+            </label>
+            <label className="form-label" style={{ marginLeft: '1rem' }}>
+              <input
+                type="radio"
+                name="contactPreference"
+                value="Email"
+                checked={contactPreference === 'Email'}
+                onChange={(e) => handleContactPreferenceChange(e.target.value)}
+              />
+              Email
+            </label>
+            <label className="form-label" style={{ marginLeft: '1rem' }}>
+              <input
+                type="radio"
+                name="contactPreference"
+                value="No I don't want to be contacted"
+                checked={contactPreference === 'No I don\'t want to be contacted'}
+                onChange={(e) => handleContactPreferenceChange(e.target.value)}
+              />
+              I don't want to be contacted
+            </label>
+          </div>
+          <br />
+          <button type="submit" className="form-button">Submit</button>
+        </form>
+      </div>
+    </>
   );
 }
 
