8ba5f7597f3061c0f8943e3941d78be415587883
Linh <57379191+linh-pham19@users.noreply.github.com>
2025-04-14 20:52:05 -0400

    Add resume viewing, and linkedin and github links (#9)
    * added resume and github link button
    
    * added resume page, added links to linkedin and github

--- personal-website/src/components/Navbar.tsx
+++ personal-website/src/components/Navbar.tsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
 import '../App.css'
 
+
 function Navbar() {
     return (
         <div className="navbar">
             <ul className='navbar-list'>
                 <li className='navbar-item'>
                     <Link to="/">Home</Link>
+                </li>
+                <li className="navbar-item">
+                    <Link to="/resume">My Resume</Link>
                 </li>
                 <li className='navbar-item'>
                     <Link to="/projects">Projects</Link>
@@ -18,8 +22,60 @@ function Navbar() {
                     <Link to="/contact">Contact me</Link>
                 </li>
             </ul>
-        </div>
+                <a
+                    href="https://github.com/linh-pham19"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                        display: 'inline-flex',
+                        alignItems: 'center',
+                        padding: '10px 20px',
+                        backgroundColor: 'transparent',
+                        color: 'white',
+                        textDecoration: 'none',
+                        borderRadius: '5px',
+                        gap: '8px'
+                    }}
+                >
+                    <img
+                        src="/GitHubFavicon.png"
+                        alt="Favicon"
+                        style={{
+                            width: '30px',
+                            height: '30px',
+                        }}
+                    />
 
+                    View My GitHub
+                </a>
+
+                <a
+                    href="www.linkedin.com/in/linh-p"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                        display: 'inline-flex',
+                        alignItems: 'center',
+                        padding: '10px 20px',
+                        backgroundColor: 'transparent',
+                        color: 'white',
+                        textDecoration: 'none',
+                        borderRadius: '5px',
+                        gap: '8px'
+                    }}
+                >
+                    <img
+                        src="/linkedinlogo.png"
+                        alt="Favicon"
+                        style={{
+                            width: '30px',
+                            height: '30px',
+                        }}
+                    />
+
+                    View My LinkedIn
+                </a>
+        </div>
     )
 }
 
