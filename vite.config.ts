ef12702f900b7d70eab2df569ef926fd8d0d73c8
Linh <linhpham0519@gmail.com>
2025-04-15 20:32:45 -0400

    added all base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
   build: {
     outDir: 'build', // Output directory for the build
   },
